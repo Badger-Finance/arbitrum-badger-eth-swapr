0bdcf402b82a470ca1ea6a1b7ef1e775109bea15
Ayush Shukla <shuklaayush247@gmail.com>
2022-01-24 22:49:27 +0530

    fix(test): test on live contract addresses

--- tests/test_upgrade.py
+++ tests/test_upgrade.py
@@ -2,26 +2,40 @@ import brownie
 import pytest
 from brownie import MyStrategy
 
+
 @pytest.fixture
 def strat_proxy(Contract):
     yield Contract.from_explorer("0x22F340C2604Dc1cDBe26caC5838Ea9EBC8862a46")
 
 
 @pytest.fixture
+def dev(accounts):
+    yield accounts[0]
+
+
+@pytest.fixture
+def new_logic(Contract):  # MyStrategy, dev
+    # logic = MyStrategy.deploy({"from": dev})
+    logic = Contract.from_explorer("0xdD2f894375CdC8E4E574F0ccB9B65D617CEafee5")
+    yield logic
+
+
+@pytest.fixture
 def proxy_admin(strat_proxy, web3, Contract):
     # ADMIN_SLOT = bytes32(uint256(keccak256("eip1967.proxy.admin")) - 1)
-    ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103
+    ADMIN_SLOT = 0xB53127684A568B3173AE13B9F8A6016E243E63B6E8EE1178D6A717850B5D6103
     yield Contract(web3.eth.getStorageAt(strat_proxy.address, ADMIN_SLOT)[12:])
 
 
-def test_upgrade(accounts, interface, strat_proxy, proxy_admin):
-    deployer = accounts[0]
+def test_upgrade(interface, strat_proxy, new_logic, proxy_admin):
     controller = interface.IController(strat_proxy.controller())
     want = interface.IERC20(strat_proxy.want())
     vault = interface.ISett(controller.vaults(want))
 
     # Verify no pending rewards
-    old_staking_contract = interface.IERC20StakingRewardsDistribution(strat_proxy.stakingContract())
+    old_staking_contract = interface.IERC20StakingRewardsDistribution(
+        strat_proxy.stakingContract()
+    )
 
     rewards = old_staking_contract.claimableRewards(strat_proxy)
     print(f"Pending rewards: {rewards}")
@@ -34,13 +48,18 @@ def test_upgrade(accounts, interface, strat_proxy, proxy_admin):
     strat_balance = strat_proxy.balanceOf()
     vault_balance = want.balanceOf(vault)
 
+    print("Balances before upgrade:")
+    print(f"Vault balance: {want.balanceOf(vault)}")
+    print(f"Strategy balance: {strat_proxy.balanceOf()}")
+    print(f"Strategy pool balance: {strat_proxy.balanceOfPool()}")
+
     assert strat_pool_balance > 0
 
     with brownie.reverts("SRD23"):
         strat_proxy.withdrawAll({"from": controller})
 
     ## Storage layout
-    governance = strat_proxy.governance()         
+    governance = strat_proxy.governance()
     strategist = strat_proxy.strategist()
     keeper = strat_proxy.keeper()
 
@@ -55,14 +74,13 @@ def test_upgrade(accounts, interface, strat_proxy, proxy_admin):
     stakingContract = strat_proxy.stakingContract()
 
     ## Upgrade
-    new_logic = MyStrategy.deploy({"from": deployer})
     owner = proxy_admin.owner()
     proxy_admin.upgrade(strat_proxy, new_logic, {"from": owner})
     print(f"Proxy admin: {proxy_admin.address}")
     print(f"Proxy admin owner: {owner}")
 
     ## Verify storage layout
-    assert governance == strat_proxy.governance()         
+    assert governance == strat_proxy.governance()
     assert strategist == strat_proxy.strategist()
     assert keeper == strat_proxy.keeper()
 
@@ -72,7 +90,9 @@ def test_upgrade(accounts, interface, strat_proxy, proxy_admin):
     assert withdrawalFee == strat_proxy.withdrawalFee()
     assert controller.address == strat_proxy.controller()
     assert guardian == strat_proxy.guardian()
-    assert withdrawalMaxDeviationThreshold == strat_proxy.withdrawalMaxDeviationThreshold()
+    assert (
+        withdrawalMaxDeviationThreshold == strat_proxy.withdrawalMaxDeviationThreshold()
+    )
 
     assert lpComponent == strat_proxy.lpComponent()
     assert reward == strat_proxy.reward()
@@ -81,8 +101,18 @@ def test_upgrade(accounts, interface, strat_proxy, proxy_admin):
     assert strat_balance == strat_proxy.balanceOf()
     assert strat_pool_balance == strat_proxy.balanceOfPool()
 
+    print("Balances after upgrade:")
+    print(f"Vault balance: {want.balanceOf(vault)}")
+    print(f"Strategy balance: {strat_proxy.balanceOf()}")
+    print(f"Strategy pool balance: {strat_proxy.balanceOfPool()}")
+
     # Check withdrawAll works
     strat_proxy.withdrawAll({"from": controller})
 
     assert strat_proxy.balanceOf() == 0
     assert want.balanceOf(vault) == vault_balance + strat_balance
+
+    print("Balances after calling withdrawAll:")
+    print(f"Vault balance: {want.balanceOf(vault)}")
+    print(f"Strategy balance: {strat_proxy.balanceOf()}")
+    print(f"Strategy pool balance: {strat_proxy.balanceOfPool()}")
